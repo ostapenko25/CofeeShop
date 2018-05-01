875fbd9542ee87c3d7aabc7ec049884eca445027
Yulia Ostapenko <ostapenko25@ukr.net>
2018-05-01 12:39:23 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,63 +1,27 @@
+
 // Menu filter
-var grid = document.getElementById('grid');
-var btnAll = document.getElementById('show-all');
-var btnAmer = document.getElementById('show-amer');
-var btnLatte = document.getElementById('show-latte');
-var btnFrappe = document.getElementById('show-frappe');
+var itemEls = document.getElementById('item-elems');
+var filterEls = document.getElementById('filter-elems');
+var filterMenu = filterEls.getElementsByClassName('menu__tag');
 
-var allCoffee = document.getElementsByClassName('grid-item');
-var amer = document.getElementsByClassName('amer');
-var latte = document.getElementsByClassName('latte');
-var frappe = document.getElementsByClassName('frappe');
-
-function showAll() {	
-	for (var i=0; i<allCoffee.length; i++) {		
-	  allCoffee[i].style.display = 'block';	  	 
-	}	
+for (var i=0; i<filterMenu.length; i++) {
+  var items = itemEls.getElementsByClassName('grid-item');
+  filterMenu[i].addEventListener('click', filt);
 }
 
-function showAmer() {	  
-	  for (var i=0; i<amer.length; i++) {
-	  amer[i].style.display = 'block';
-	   }
-	  for (var i=0; i<latte.length; i++) {
-	  latte[i].style.display = 'none';	  
-	   }
-	  for (var i=0; i<frappe.length; i++) {
-	  frappe[i].style.display = 'none';
-	   }
-	 }
+function filt() {
+  var items = itemEls.getElementsByClassName('grid-item');
+  for (var i=0; i<items.length; i++) {
+  	if (items[i].classList.contains(this.value)) {
+    	items[i].style.display = 'block';
+    } else {
+    	items[i].style.display = 'none';
+    }
+  }
+} 
 
-function showLatte() {		
-	   for (var i=0; i<amer.length; i++) {
-	  amer[i].style.display = 'none';
-	   }
-	  for (var i=0; i<latte.length; i++) {
-	  latte[i].style.display = 'block';	 
-	   }
-	  for (var i=0; i<frappe.length; i++) {
-	  frappe[i].style.display = 'none';
-	   }
-	 }
 
-function showFrappe() {	  
-	   for (var i=0; i<amer.length; i++) {
-	  amer[i].style.display = 'none';
-	   }
-	  for (var i=0; i<latte.length; i++) {
-	  latte[i].style.display = 'none';	  
-	   }
-	  for (var i=0; i<frappe.length; i++) {	
-	  frappe[i].style.display = 'block';
-	  }
-	 }
-
-btnAll.addEventListener('click', showAll);
-btnAmer.addEventListener('click', showAmer);
-btnLatte.addEventListener('click', showLatte);
-btnFrappe.addEventListener('click', showFrappe);
-
-//Promo banners
+////Promo banners
 var promoImg1 = document.getElementById('promoimg1');
 var promoImg2 = document.getElementById('promoimg2');
 var promoImg3 = document.getElementById('promoimg3');
@@ -89,33 +53,3 @@ function showPromo3() {
 	promoImg2.style.display = 'none';
 	promoImg3.style.display = 'block';
 	}
-
-// Fact-History
-var factBtn = document.getElementById('btn-fact');
-var histBtn = document.getElementById('btn-hist');
-var factText = document.getElementById('facts-text');
-var histText = document.getElementById('hist-text');
-
-factBtn.addEventListener('click', showFact);
-histBtn.addEventListener('click', showHist);
-
-function showFact() {	 	
-	factText.style.display = 'block';
-	factText.style.transition = 'all 0.3s ease';
-	factBtn.classList.add('active');
-	
-	histText.style.display = 'none';
-	histBtn.classList.remove('active');
-	}
-
-function showHist() {	 
-	histText.style.display = 'block';
-	histText.style.transition = 'all 0.3s ease';
-	histBtn.classList.add('active');
-	
-	factText.style.display = 'none';
-	factBtn.classList.remove('active');		
-	}
-
-
-
